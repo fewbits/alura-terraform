3273f98b510026896e52f3a392e67457f0def0cf
Eric Guimarães de Sousa Silva <eric.gssilva@gmail.com>
2020-04-18 16:56:49 -0300

    Aula 02 - Security group

--- main.tf
+++ main.tf
@@ -12,4 +12,24 @@ resource "aws_instance" "dev" {
   tags = {
       Name = "dev${count.index}"
   }
+
+  vpc_security_group_ids = ["sg-0b0c7b8ed1621e1b6"]
 }
+
+resource "aws_security_group" "acesso_ssh" {
+  name        = "acesso_ssh"
+  description = "acesso_ssh"
+  # vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    # description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["191.165.196.166/32"] 
+  }
+
+  tags = {
+    Name = "acesso_ssh"
+  }
+} 
